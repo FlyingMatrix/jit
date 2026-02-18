d4fe8de8e9590f1ecc001aaf82b3a17900461e3f
FlyingMatrix <jxzhang.acm@gmail.com>
2026-02-18 11:56:35 +0100

    latest update

---
+++ denoiser.py
@@ -0,0 +1,10 @@
+import torch 
+import torch.nn as nn
+
+class Denoiser(nn.Module):
+    def __init__(self, args):
+        pass
+
+    @torch.no_grad()
+    def update_ema(self):
+        pass
--- engine.py
+++ engine.py
@@ -33,6 +33,37 @@ def train_one_epoch(model, model_without_ddp, data_loader, optimizer, device, ep
         x = x * 2.0 - 1.0   # from [0, 1] to [-1, 1]
         labels = labels.to(device, non_blocking=True)
 
+        with torch.amp.autocast('cuda', dtype=torch.bfloat16):  # enable automatic mixed precision (AMP), run autocast on GPU, use bfloat16 precision for supported ops
+            loss = model(x, labels)
+
+        loss_value = loss.item()
+        if not math.isfinite(loss_value):   # when loss_value == NaN, +inf, -inf
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)     # stop training and handle it
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        torch.cuda.synchronize()    # block the CPU until all queued CUDA operations on the GPU are finished
+        model_without_ddp.update_ema()  # update the EMA of the model parameters to smooth or stabilize the model's weights over training steps
+        
+        # add loss_value and lr into deque (double-ended queue)
+        metric_logger.update(loss=loss_value)
+        lr = optimizer.param_groups[0]["lr"]
+        metric_logger.update(lr=lr)
+
+        # calculate mean value across multiple GPUs (processes)
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+
+        if log_writer is not None:
+            pass
+
+
+
+
+
+
         
 
     
