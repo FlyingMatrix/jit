ecea97f6e5e83a79ce7bdd8d0a696eb0a0528266
FlyingMatrix <jxzhang.acm@gmail.com>
2026-02-22 11:53:48 +0100

    latest update

--- utils/model_util.py
+++ utils/model_util.py
@@ -4,6 +4,19 @@ import numpy as np
 from math import pi
 from einops import rearrange, repeat    # used to reshape, permute, flatten, split, combine or repeat tensor dimensions in a very readable way
 
+def broadcat(tensors, dim=-1):  # operation dimension: dim=-1
+    num_tensors = len(tensors)
+    shape_lens = {len(t.shape) for t in tensors}
+    assert len(shape_lens) == 1, 'tensors must all have the same number of dimensions'
+    shape_len = list(shape_lens)[0]
+    dim = (dim + shape_len) if dim < 0 else dim     # convert the negative index of dimensions to its positive equivalent
+    dims = list(zip(*(t.shape for t in tensors)))   # the * unpacks the list so zip works column-wise, final results contain all tensor sizes for a specific dimension index
+    expandable_dims = [(i, val) for i, val in enumerate(dims) if i != dim]  # identify expandable dimensions: get all dimensions and sizes except the last dimension
+    assert all(len(set(val)) == 1 or 1 in val for _, val in expandable_dims), "invalid dimensions for broadcastable concatenation"
+    
+
+
+
 class VisionRotaryEmbedding(nn.Module):
     pass
 
