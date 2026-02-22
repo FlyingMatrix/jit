e17162408fa8b566473eb91741e1c26c1bda6c6e
FlyingMatrix <jxzhang.acm@gmail.com>
2026-02-22 14:06:26 +0100

    latest update

--- utils/model_util.py
+++ utils/model_util.py
@@ -4,7 +4,10 @@ import numpy as np
 from math import pi
 from einops import rearrange, repeat    # used to reshape, permute, flatten, split, combine or repeat tensor dimensions in a very readable way
 
-def broadcast(tensors, dim=-1):  # operation dimension: dim=-1
+def broadcast(tensors, dim=-1):
+    """
+        broadcast tensors and concatenate along the chosen dimension
+    """
     num_tensors = len(tensors)
     shape_lens = {len(t.shape) for t in tensors}
     assert len(shape_lens) == 1, 'tensors must all have the same number of dimensions'
@@ -15,8 +18,14 @@ def broadcast(tensors, dim=-1):  # operation dimension: dim=-1
     assert all(len(set(val)) == 1 or 1 in val for _, val in expandable_dims), "invalid dimensions for broadcastable concatenation"
     max_dims = [(i, max(val)) for i, val in expandable_dims]    # sizes for broadcastable dimensions
     expanded_dims = [(i, (size,) * num_tensors) for i, size in max_dims]
-    
+    expanded_dims.insert(dim, (dim, dims[dim]))     # now expanded_dims contains all dimensions
+    expandable_shapes = list(zip(*(sizes for _, sizes in expanded_dims)))
+    tensors = list(map(lambda t: t[0].expand(*t[1]), zip(tensors, expandable_shapes)))
+    tensors = [tensor.expand(*shape) for tensor, shape in zip(tensors, expandable_shapes)]  # broadcast the tensor to the target shape
+    return torch.cat(tensors, dim = dim)    # concatenate along the chosen dimension
 
+def rotate_half(x):
+    pass
 
 
 class VisionRotaryEmbedding(nn.Module):
