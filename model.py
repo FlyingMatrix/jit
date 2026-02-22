1b67c50756b468cb7d4529006c85f4c39473e3f4
FlyingMatrix <jxzhang.acm@gmail.com>
2026-02-22 02:41:54 +0100

    latest update

---
+++ model.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+import math
+import torch.nn.functional as F
+from utils.model_util import VisionRotaryEmbeddingFast, RMSNorm
+
---
+++ utils/model_util.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+import numpy as np
+from math import pi
+from einops import rearrange, repeat    # used to reshape, permute, flatten, split, combine or repeat tensor dimensions in a very readable way
+
+class VisionRotaryEmbedding(nn.Module):
+    pass
+
+class VisionRotaryEmbeddingFast(nn.Module):
+    pass
+
+class RMSNorm(nn.Module):
+    pass
