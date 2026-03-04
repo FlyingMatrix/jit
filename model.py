9ba907d3217e4dec0249c00f71ca22197cd6c294
FlyingMatrix <jxzhang.acm@gmail.com>
2026-03-04 16:42:04 +0100

    latest update

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ def modulate(x, shift, scale):
     output = x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
     return output
 
-class BottleneckPatchEmbed(nn.Module):  # image to patch embedding
+class BottleneckPatchEmbedder(nn.Module):  # image to patch embedding
     def __init__(self, img_size=224, patch_size=16, in_channels=3, pca_dim=768, embed_dim=768, bias=True):
         """
             - img_size: size of input image
@@ -47,7 +47,40 @@ class BottleneckPatchEmbed(nn.Module):  # image to patch embedding
         x = x.flatten(2).transpose(1, 2)    # x.shape = (batch_size, num_patches, embed_dim)
         return x    
     
+class TimestepEmbedder(nn.Module):  # scalar timesteps embedding
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super.__init__()
+        
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
 
-
+        @staticmethod
+        def timestep_embedding(t, dim, max_period=10000):
+            """
+                 input: a tensor of timesteps t of shape (batch_size,)
+                output: an embedding vector of shape (batch_size, dim)
+            """
+            # calculate frequencies based on the function: freqs = max_period ** (-index / half), which is from openai -> glide_text2im/nn.py
+            half = dim // 2
+            index = torch.arange(start=0, end=half, dtype=torch.float32)     # index -> (dim//2,)
+            freqs = max_period ** (-index / half).to(device=t.device)        # freqs -> (dim//2,)
+            # multiply by timesteps
+            args = t[:, None].float() * freqs[None]     # (batch_size, 1) * (1, dim//2) -> (batch_size, dim//2)
+            # concatenate
+            embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)   # embedding.shape = torch.Tensor(batch_size, dim)
+            # in odd dimension case, one zero column is appended
+            if dim % 2: # in odd dimension case, embedding.shape = torch.Tensor(batch_size, dim-1)
+                embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)  # now, embedding.shape = torch.Tensor(batch_size, dim)
+            return embedding    # embedding.shape = torch.Tensor(batch_size, dim)
+        
+        def forward(self, t):   # t.shape = (batch_size,)
+            t_embedding = self.timestep_embedding(t, self.frequency_embedding_size)  # t_embedding.shape = torch.Tensor(batch_size, self.frequency_embedding_size)
+            # pass the t_embedding through MLP to make it can be learned by the model
+            t_embedding = self.mlp(t_embedding)
+            return t_embedding  # t_embedding.shape = torch.Tensor(batch_size, hidden_size)
 
 
